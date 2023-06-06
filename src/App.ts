d287b03b83750d171ef31fbaeda0d47eb9de231d
Edward Tanguay (windows) <edward@tanguay.info>
2023-06-06 15:53:16 +0200

    footer in

--- src/App.ts
+++ src/App.ts
@@ -11,4 +11,21 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML =/* html */`
 <main>
 	${getCurrentPage()}
 </main>
+
+<footer>
+	<div class="impressum">
+	<h1 class="h1-foot"> Impressum</h1>
+	<p> Veratwortlich für den Inhalt:
+	Lutz Kühlmann
+	Dipl. Ingenieur Architektur.<br/>
+	Händelstraße,19<br/>
+	76185, Karlsruhe<br/></p>
+</div>
+<div class="foot">
+	<a href="E-mail:info@kuehlmann-projektleitung.de">info@kuehlmann-projektleitung.de </a>
+	<a href="https://kuehlmann-projektleitung.de/">www.kuehlmann-projektleitung.de </a>
+
+	<p>Authorin:Dci Studentin</p> 
+</div>
+</footer>
 `;
